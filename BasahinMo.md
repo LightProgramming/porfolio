e57c555d88b7bf5510a10fbd9c677a0cd14e518e
Light Senpai <vergarajamesryan47@gmail.com>
2024-07-25 21:41:28 -0700

    responsive for smaller device modified

--- BasahinMo.md
+++
@@ -1,5 +0,0 @@
-Patch 1.0
--James -> Nagdagdag ng additional feature sa call to action
-
-Patch v.123
--Ian & James -> Tapos na ang coding
--- javascript/htmlscript.js
+++ javascript/htmlscript.js
@@ -2,6 +2,6 @@ let menuIcon = document.querySelector('#menu-icon');
 let navbar = document.querySelector('.navbar');
 
 menuIcon.onclick = () => {
-    menuIcon.classList.toggle('bx-x')
+    menuIcon.classList.toggle('fa-solid-x')
     navbar.classList.toggle('active');
 }
